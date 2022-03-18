2fd3873813609f5ebf5c05d9d15dfebb3e86485c
Safer68 <Safer@yandex.ru>
2022-03-19 00:47:21 +0300

    Initial commit

---
+++ src/main/java/Lesson3/Task.java
@@ -0,0 +1,13 @@
+package Lesson3;
+
+/**
+ * Hello world!
+ *
+ */
+public class Task
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
